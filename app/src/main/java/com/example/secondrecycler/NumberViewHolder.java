a4c0a7f5080956810ef19b1b0e074d952f8b9e90
princessnuri <nurgaeva1606@gmail.com>
2022-07-19 18:44:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/secondrecycler/NumberViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.secondrecycler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NumberViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+    public NumberViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView=itemView.findViewById(R.id.textview_number);
+    }
+
+    public void bind(String number){
+        textView.setText(number);
+    }
+}
